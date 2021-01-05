31a9bafc150cd2ad68a1eeef30e3374ce7193146
Adrien Moulin <adrien.moulin@airy3d.com>
2021-01-04 20:14:20 -0500

    first testing working with OCLRadixSort

--- app/ParticleSystemApp.cpp
+++ app/ParticleSystemApp.cpp
@@ -10,6 +10,8 @@
 
 #include "Boids.hpp"
 
+#include "RadixSortCommon.hpp"
+
 #if __APPLE__
 constexpr auto GLSL_VERSION = "#version 150";
 #else
@@ -62,6 +64,45 @@ bool ParticleSystemApp::initWindow()
   ImGuiIO& io = ImGui::GetIO();
   (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+  //
+  //
+  //
+
+  // create instance for device
+  SimpleRadixSort64bit rs { getDevice() };
+
+  std::vector<SimpleRadixSort64bit::KeyType> v(1 << 26);
+  std::cout << "generating " << v.size() << " keys...." << std::endl;
+
+  auto rng = makeRng(rs.maxValue());
+  std::generate(v.begin(), v.end(), rng);
+
+  // take a copy of v for permutation check
+  std::vector<SimpleRadixSort64bit::KeyType> vCopy = v;
+
+  // try
+  {
+    // sort method is STL-style
+    rs.sort(v.begin(), v.end());
+  }
+  // catch (const cl::Error& e)
+  {
+    //   std::cerr << "CL Error: " << e.err() << " " << e.what() << std::endl;
+    //   return 1;
+  }
+
+  std::cout << "Sorted: " << std::boolalpha << std::is_sorted(v.begin(), v.end()) << std::endl;
+  std::cout << "Permutation correct: " << std::boolalpha << checkPermutation(v, vCopy, rs.permutation()) << std::endl;
+
+  const auto pi = rs.profilingInfo();
+  std::cout << "Profiling: " << std::endl;
+
+  std::cout << "total kernel execution time: " << pi.total() * 1e-6 << "ms" << std::endl;
+
+  //
+  //
+  //
 
   return true;
 }
---
+++ physics/RadixSortCommon.hpp
@@ -0,0 +1,64 @@
+#ifndef OCLRADIXSORT_TESTCOMMON_HPP_H
+#define OCLRADIXSORT_TESTCOMMON_HPP_H
+
+#include <algorithm>
+#include <chrono>
+#include <random>
+
+#include "RadixSort.hpp"
+
+// change this to select desired platform and device for all tests
+#define PLATFORM_DEFAULT_ID 0
+#define DEVICE_DEFAULT_ID 0
+
+// helper function for obtaining CL device
+cl::Device getDevice(int pid = PLATFORM_DEFAULT_ID, int did = DEVICE_DEFAULT_ID, cl_uint type = CL_DEVICE_TYPE_GPU)
+{
+  std::vector<cl::Platform> pls;
+  cl::Platform::get(&pls);
+
+  std::vector<cl::Device> dvs;
+  pls[pid].getDevices(type, &dvs);
+
+  for (auto& d : dvs)
+  {
+    std::cout << d.getInfo<CL_DEVICE_NAME>() << ' ' << d.getInfo<CL_DEVICE_VERSION>() << std::endl;
+  }
+
+  return dvs[did];
+}
+
+// helper function to check the correctness of permutation
+template <typename T, typename U>
+bool checkPermutation(const std::vector<T>& keysAfterSort,
+    const std::vector<T>& keysBeforeSort,
+    const std::vector<U>& permutation)
+{
+  for (size_t i = 0; i < keysAfterSort.size(); ++i)
+  {
+    if (keysBeforeSort[permutation[i]] != keysAfterSort[i])
+    {
+      std::cout << i << " " << keysAfterSort[i] << " " << keysBeforeSort[i] << " " << permutation[i] << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+template <typename T>
+auto makeRng(T upperBound)
+{
+  return std::linear_congruential_engine<T, std::minstd_rand::multiplier, std::minstd_rand::increment, std::minstd_rand::modulus> {
+    static_cast<T>(std::chrono::steady_clock::now().time_since_epoch().count())
+  };
+}
+
+// sample Radix Sort configuration
+// type of keys
+using T = unsigned /*long long*/ int;
+// type of indices
+using TI = T;
+// radix sort template configuration
+using SimpleRadixSort64bit = RadixSort<8, 32, T, TI, 128, 4, true, 256, false, 32, true>;
+
+#endif //OCLRADIXSORT_TESTCOMMON_HPP_H
