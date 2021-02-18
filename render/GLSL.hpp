4a4c02360266e32b6bb9301196f312d38382dd4f
Adrien Moulin <adrien.moulin@airy3d.com>
2021-02-18 11:30:24 -0500

    point size depending on the dist to camera

--- render/GLSL.hpp
+++ render/GLSL.hpp
@@ -14,7 +14,10 @@ constexpr char PointCloudVertShader[] = R"(#version 330 core
     {
         vertexColor = vec4(aCol, 1.0);
         gl_Position = u_projView * vec4(aPos, 1.0);
-        gl_PointSize = 1.0;
+
+        vec4 eye = u_projView * vec4(aPos, 1.0); 
+        float d = length(eye);
+        gl_PointSize = max(3000.0 * 1.0/(0.04 + 0.8*d + 0.0002*d*d), 0.8); 			
     }
     )";
 
